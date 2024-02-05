060eb05db80d5495484bc3d5de4ac7dbc5c200d9
arimax32 <dattaaritra2002@gmail.com>
2024-02-05 16:30:44 +0530

    Added Video Management endpoints for CRUD

--- Backend/ApiServer/controllers/videoController.js
+++ Backend/ApiServer/controllers/videoController.js
@@ -1,4 +1,5 @@
 const { decrypt, secureVideos } = require('../utils/encryption');
+const prisma = require("./data/db")
 
 const getAllVideos = async (req, res) => {
     const videos = await prisma.video.findMany({
--- Backend/ApiServer/server.js
+++ Backend/ApiServer/server.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const express = require('express')
 const path = require('path');
 const cors = require('cors');
-const prisma = require("./data/db")
 const videoRouter = require("./routes/videoRoutes")
 
 const app = express()
